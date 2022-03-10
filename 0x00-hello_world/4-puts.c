0c13a02c9f453ddee9f34dc37c59b61c9d7089f7
kosi uhuegbu <kosiuhuegbu@gmail.com>
2022-03-10 14:09:07 +0100

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-This function will print the following string using puts.
+ * Return: return 0
+ */
+int main(void)
+{
+char str[80] = "\"Programming is like building a multilingual puzzle";
+puts(str);
+return (0);
+}
