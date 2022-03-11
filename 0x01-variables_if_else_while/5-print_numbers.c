9c4d51938ca96220e190a04c37c09e49c553bb04
kosi uhuegbu <kosiuhuegbu@gmail.com>
2022-03-11 14:22:26 +0100

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+
+  
+#include <stdio.h>
+
+/**
+ * main - Will print the numbers from 0 to 9
+ *
+ * Return: Always returns 0 (success)
+ */
+
+int main(void)
+{
+	int num[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int i;
+
+	for
+		(i = 0;
+		 i < 10;
+		 i++)
+	{
+		printf("%d", num[i]);
+	}
+	printf("\n");
+	return (0);
+}
