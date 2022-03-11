f5a5ccc40a39c5da234cf7f35d61d57e51a67a2e
kosi uhuegbu <kosiuhuegbu@gmail.com>
2022-03-11 14:45:35 +0100

    task 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two two-digit numbers.
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+int i, j, l, k;
+
+for (i = 0; i <= 9; i++)
+{
+	for (j = 0; j <= 9; j++)
+	{
+		for (l = 0; l <= 9; l++)
+		{
+			for (k = 0; k <= 9; k++)
+			{
+				if (i < l || (i == l && j < k))
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(l + '0');
+					putchar(k + '0');
+					if (!(i == 9 && j == 8 && l == 9 && k == 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
