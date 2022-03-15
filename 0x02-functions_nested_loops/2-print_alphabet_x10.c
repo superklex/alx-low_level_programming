6bfd601aa630e0d114f25b1278dc1f290f4b8c17
kosi uhuegbu <kosiuhuegbu@gmail.com>
2022-03-15 15:07:29 +0100

    02

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,32 @@
+
+  
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet ten times
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char a;
+	int i;
+
+for
+(i = 0;
+i < 10;
+i++)
+{
+for
+(a = 'a';
+a <= 'z';
+a++)
+{
+
+_putchar(a);
+
+}
+
+_putchar('\n');
+}
+}
