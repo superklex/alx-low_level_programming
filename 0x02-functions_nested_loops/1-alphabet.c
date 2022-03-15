19d832d83cb2a3f72dee03df75fb4da51a613a26
kosi uhuegbu <kosiuhuegbu@gmail.com>
2022-03-15 15:04:14 +0100

    01

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - will print out lower cased alphabet
+ *
+ */
+
+void print_alphabet(void)
+{
+	char a;
+
+	for
+		(a = 'a';
+		 a <= 'z';
+		 a++)
+	{
+	_putchar(a);
+	}
+	_putchar('\n');
+}
