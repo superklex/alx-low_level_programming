e5423496d7b7e66f78f15f60e43a2843ee8643a5
kosi uhuegbu <kosiuhuegbu@gmail.com>
2022-04-07 13:05:49 +0100

    task 1

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,47 +2,42 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - function that concatenates two strings
- * @s1: input string 1
- * @s2: input string 2
- * @n: first n bytes
+ * _calloc - function that allocates memory for an array using malloc
+ * @nmemb: input size 1
+ * @size: input size bytes
+ * Return:nothing
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *cal;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	cal = malloc(nmemb * size);
+	if (cal == NULL)
+		return (NULL);
+	_memset(cal, 0, nmemb * size);
+	return (cal);
+}
+/**
+ * _memset - function that fills memory with a constant byte.
+ *
+ * @s: memory area to return
+ * @b: constant bytes
+ * @n: size of bytes
  * Return: char
  */
 
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
-	char *str;
-	char *nul = "";
-	unsigned int x, y;
+	unsigned int i = 0;
 
-	x = y = 0;
-	if (s1 == NULL)
-		s1 = nul;
-	if (s2 == NULL)
-		s2 = nul;
-	while (s1[x] != '\0')
-		x += 1;
-	while (s2[y] != '\0')
-		y += 1;
-	y++;
-	if (n >= y)
-		n = y;
-	str = malloc(sizeof(*str) * n + (x + 1));
-	if (str == NULL)
-		return (NULL);
-	x = 0;
-	while (s1[x] != '\0')
+	while (i < n)
 	{
-		str[x] = s1[x];
-		x += 1;
+
+		*(s + i) = b;
+		i += 1;
 	}
-	y = 0;
-	while (s2[y] != '\0' && y < n)
-	{
-		str[x] = s2[y];
-		x++;
-		y++;
-	}
-	str[x] = '\0';
-	return (str);
+	return (s);
 }
